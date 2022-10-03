83150f45e2fd5b4f0d5a701a13df2f2c3fc13b3d
Pabl0Magic <pablo.almarza@estudiante.uam.es>
2022-10-03 17:11:41 +0200

    PDF included

---
+++ algo_p1/graphs05.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+from timeit import timeit
+from .p105import p105
+
+def plot1():
+    l_timings = []
+    for i in range(11):
+        dim = 10+i
+        h = list(np.random.permutation(dim))
+        timings = %timeit -o -n 10 -r 5 -q create_min_heap(h)
+        l_timings.append([dim, timings.best])
+
+    print(l_timings)
