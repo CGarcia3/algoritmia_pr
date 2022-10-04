9b3b3d92072667f98de552a03f78ed3fffc90c20
CGarcia3 <carlos.garciatoledano@estudiante.uam.es>
2022-10-04 12:29:14 +0200

    Cambios a p105.py para pdoc

--- algo_p1/p105/p105.py
+++ algo_p1/p105/p105.py
@@ -5,10 +5,12 @@ from typing import Callable
 
 """ I-A. Midiendo tiempos con %timeit """
 
-"""
-Function that receives two matrix and perform the multiplication
-"""
+
 def matrix_multiplication(m_1: np.ndarray, m_2: np.ndarray)-> np.ndarray:
+    """
+    Function that receives two matrix and perform the multiplication, then returns the result
+    """ 
+    
     """Check the dimensions to see if they can be multiplied"""
     if(m_1.shape[1]!=m_2.shape[0]):
         return None
@@ -25,25 +27,15 @@ def matrix_multiplication(m_1: np.ndarray, m_2: np.ndarray)-> np.ndarray:
             res[i][j] = sum
 
     return res
-"""
-if __name__=="__main__":
-    l_timings = []
-    
-    for i in range(11):
-        dim = 10+i
-        m = np.random.uniform(0., 1., (dim, dim))
-        timings = %timeit -o -n 10 -r 5 -q matrix_multiplication(m, m)
-        l_timings.append([dim, timings.best])
-
-    print(l_timings)
-"""
 
 """ I-B. Busqueda binaria """
 
-"""
-Recursive version of binary search
-"""
+
 def rec_bb(t: list, f: int, l: int, key: int)-> int:
+    """
+    Recursive version of binary search, receives the complete list,
+    the key to search, and the lower and upper limits of the list
+    """
     if f>l:
         return None
 
@@ -60,10 +52,11 @@ def rec_bb(t: list, f: int, l: int, key: int)-> int:
         return rec_bb(t, f, index-1, key)
 
 
-"""
-Iterative version of binary search
-"""  
 def bb(t: list, f: int, l: int, key: int)-> int:
+    """
+    Iterative version of binary search, receives the complete list,
+    the key to search, and the lower and upper limits of the list
+    """  
     if f>l:
         return None
 
@@ -82,55 +75,16 @@ def bb(t: list, f: int, l: int, key: int)-> int:
     
     return index
 
-"""
-def script2():
-    l_times = []
-    for i, size in enumerate(range(5, 15)):
-        t = list(range(2**i * size))
-        key = ...
-        timings = %timeit -n 100 -r 10 -o -q ...(t, 0, len(t) - 1, key)
-        l_times.append([len(t), timings.best])
-        times = np.array(l_times)
-    print(l_times)
-"""    
-
 """I-C. Cuestiones"""
 
-
-def fit_func_2_times(timings: np.ndarray, func_2_fit: Callable):
-    """Ajusta linealmente los valores de la funcion func_2_fit a
-    los tiempos en timings.
-    Esto es, calculamos valores a, b para que la funcion a*f(dim) + b
-    se ajuste a los tiempos medidos.
-    """
-    """if len(timings.shape) == 1:
-        timings = timings.reshape(-1, 1)
-    values = func_2_fit(timings[ :, 0]).reshape(-1, 1)
-    
-    #normalizar timings
-    times = timings[ : , 1] / timings[0, 1]
-    
-    #ajustar a los valores en times un modelo lineal sobre los valores en values
-    lr_m = LinearRegression()
-    lr_m.fit(values, times)
-    return lr_m.predict(values)"""
-    pass
-
-def func_2_fit(n):
-
-    # IDEA: Devolver tuplas de valores de un plot que tenga en x la dimension (n), y
-    # en y el tiempo que tarda en realizar la multiplicacion
-    
-    # IDEA2: Devolver valores de tiempo
-    pass
-
+# sin hacer
 
 """II-A. Min Heaps sobre arrays de Numpy"""
 
-"""
-Function that applies heapify operation to an element in position i 
-"""
 def min_heapify(h: np.ndarray, i: int):
+    """
+    Function that applies heapify operation to an element of the heap h in position i 
+    """
     """Define the child nodes in the array"""
     left_child = 2*i+1
     right_child = 2*i+2 
@@ -149,11 +103,12 @@ def min_heapify(h: np.ndarray, i: int):
             return 
             
 
-"""
-Function that inserts an element in a min heap and 
-returns the heap with the element inserted
-"""
 def insert_min_heap(h: np.ndarray, k: int) -> np.ndarray:
+    """
+    Function that inserts an element in a min heap and 
+    returns the heap with the element inserted in its correct position
+    """
+
 
     """Check if the array is empty"""
     if h == None:
@@ -170,8 +125,10 @@ def insert_min_heap(h: np.ndarray, k: int) -> np.ndarray:
     return h
 
 
-"""Function that creates a min heap from an array"""
 def create_min_heap(h: np.ndarray):
+    """Function that creates a min heap from an array 
+    applying heapify in all its elements"""
+
     """Create an empty array"""
     aux = np.empty(len(h))
 
@@ -183,27 +140,31 @@ def create_min_heap(h: np.ndarray):
 
 """II-B. Colas de prioridad sobre min Heaps"""
 
-"""
-Function that initializes a priority queue
-"""
 def pq_ini():
+    """
+    Function that initializes a priority queue
+    """
     p_queue = []
 
-"""
-Function that inserts an element k in a priority queue
-"""
+
 def pq_insert(h: np.ndarray, k:int) -> np.ndarray:
+    """
+    Function that inserts an element k in a priority queue
+    """
+
     """Check if the array is empty"""
     if h == None:
         h=[]
     h.append(k)
     return h
 
-"""
-Function that removes the element with the least priority
-in a priority queue
-"""
 def pq_remove(h: np.ndarray) -> np.ndarray:
+    """
+    Function that removes the element with the least priority
+    in a priority queue
+    """
+
+
     """Check if the array is empty"""
     if h == None:
         return None
@@ -221,10 +182,13 @@ def pq_remove(h: np.ndarray) -> np.ndarray:
     
 """II-C. El problema de seleccion"""
 
-"""
-Function that selects the element k in a disordered array
-"""
+
 def select_min_heap(h: np.ndarray, k:int) -> int:
+    """
+    Funtion that returns the element of a disordered array that would
+    occupy the k position in the ordered version of the array
+    """
+
     """Check if the array is empty"""
     if h is None:
         return -1
